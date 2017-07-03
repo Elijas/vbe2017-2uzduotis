996cfa9859efa5f92d4d6c2f77a764d634b2cec7
Elijas <master.elijas@gmail.com>
2017-07-03 16:35:34 +0300

    read data

--- main.cpp
+++ main.cpp
@@ -5,14 +5,28 @@
 
 using namespace std;
 
+struct Rect {
+    int x, y, dx, dy, r, g, b;
+};
+
 int main() {
     // Opening files and reading metadata
     ifstream inputFile("U2.txt");
     ofstream outputFile("U2rez.txt");
     int rectCount;
     inputFile >> rectCount;
-    assert(rectCount == 3);//d
 
+    // Reading data
+    Rect rectangles[rectCount];
+    for (int i = 0; i < rectCount; ++i) {
+        Rect rect;
+        inputFile >> rect.x >> rect.y >> rect.dx >> rect.dy >> rect.r >> rect.g >> rect.b;
+        rectangles[i] = rect;
+    }
+    assert(rectangles[0].dx == 2);
+
+    // Find the size of final picture
+    int length, width;
 
 
     // Closing the program
