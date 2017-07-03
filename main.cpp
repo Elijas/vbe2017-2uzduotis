b802554699ff612f6fd945264b10d690bf26eeb5
Elijas <master.elijas@gmail.com>
2017-07-03 16:28:44 +0300

    read metadata

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, Worldd!" << std::endl;
+    // Opening files and reading metadata
+    ifstream inputFile("U2.txt");
+    ofstream outputFile("U2rez.txt");
+    int rectCount;
+    inputFile >> rectCount;
+    assert(rectCount == 3);//d
+
+
+
+    // Closing the program
+    inputFile.close();
+    outputFile.close();
     return 0;
 }
